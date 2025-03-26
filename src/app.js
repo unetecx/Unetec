a8b6b054437e9eae0d4d819e3fdd041490eb5fa5
unetecx <unetectcc@gmail.com>
2025-03-26 09:15:41 -0300

    desenvolvendo back-end login e logout

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express =  require('express');
+const sesseion = require('express-session');
+const passaport = require('passport');
+const mongo = require ('mongoose');
+const localStrategy = require('passport-local').Strategy;
+const bctypt = ('bcrypt');

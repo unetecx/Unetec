556f94db52627cae85a33e5cdf7619c934b8d9e2
unetecx <unetectcc@gmail.com>
2025-03-26 09:13:51 -0300

    organizando pastas

--- src/config/controllers/routes/app.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express');
-const sesseion = require('express-session');
-const passaport = require('passport');
-const mongo = require ('mongoose');
-const localStrategy = require('passport-local').Strategy;
-const bctypt = ('bcrypt');
-
-
